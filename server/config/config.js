32b36a71b84904cf5f6de05b6d2b3a96a920bb96
Don-Tayuelo <aljadis.tavera@sofka.com.co>
2020-11-27 10:28:37 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://akhali:SbwPtxQWcQF6NE2o@cluster0.ozuaa.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
